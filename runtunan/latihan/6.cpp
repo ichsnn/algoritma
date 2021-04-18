7f0674d474fce3d8e41240444dc7349d816ea518
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-18 16:44:42 +0700

    code

--- runtunan/latihan/6.cpp
+++ runtunan/latihan/6.cpp
@@ -5,8 +5,8 @@ int main() {
 
     //DEKLARASI
     struct satuan {
-        float inchi;  //25,44 mm =  / 1000
-        float kaki;   //30,48 cm = / 100
+        float inchi;  //25,44 mm 
+        float kaki;   //30,48 cm 
         float yard;   //0,9144 m
     };
     satuan p;
@@ -15,10 +15,7 @@ int main() {
     //ALGORITMA
     std::cout<<"Masukkan panjang benda (m) : ";
     std::cin>>panjang_benda;
-
-    //1 inchi = 25.4 mm = 1/ 25.4
-    //1 kaki = 30.38 cm = 1 /30,38
-    //1 yard 0,9144 m = 1 / 0,9144
+   
     p.inchi = panjang_benda * 1000 / 25.4;
     p.kaki = panjang_benda * 100 / 30.38;
     p.yard = panjang_benda / 0.9144;
