7e31f5c90888d6581b4aa7637118667f735eb41f
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-27 14:41:27 +0700

    code

--- pemilihan/KalkulatorSederhana.cpp
+++ pemilihan/KalkulatorSederhana.cpp
@@ -5,22 +5,21 @@ using namespace std;
 int main() {
 
     //DEKLARSI 
-    int op1, op2, ans;
+    int op1, op2;
     char oprt;
 
     //ALGORITMA
     cin>>op1>>oprt>>op2;
     switch(oprt) {
-        case '+': ans = op1+op2; break;
-        case '-': ans = op1-op2; break;
-        case '*': ans = op1*op2; break;
+        case '+': cout<<op1+op2; break;
+        case '-': cout<<op1-op2; break;
+        case '*': cout<<op1*op2; break;
         case '/': if((op1 && op2) != 0) {
-                    ans = op1/op2; break;
+                    cout<<op1/op2; break;
                   } else cout<<"error!"; break;
         
         default: cout<<"operator salah!"; break;       
-    }
-    cout<<ans;
+    }    
 
     return 0;
 }
