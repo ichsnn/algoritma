6220548f057795fc7394d7d4ce5d2a2bba1d1bdc
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-28 15:23:46 +0700

    code

---
+++ pemilihan/latihan/5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    //DEKLARASI
+    char a;
+    int b;
+
+    //ALGORITMA
+    cin>>a;
+    switch (a) {
+        case '0': b = 0; break;
+        case '1': b = 1; break;        
+        case '2': b = 2; break;
+        case '3': b = 3; break;        
+        case '4': b = 4; break;
+        case '5': b = 5; break;        
+        case '6': b = 6; break;
+        case '7': b = 7; break;        
+        case '8': b = 8; break;
+        case '9': b = 9; break;        
+        default: b=-1; break;
+   } 
+   cout<<b;
+
+   return 0;
+
+}
