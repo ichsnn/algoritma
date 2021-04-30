54e9b65b7b293b1cd9011a3181a3ac033af5a494
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-30 14:00:07 +0700

    code

---
+++ pengulangan/PeluncuranRoket.cpp
@@ -0,0 +1,20 @@
+//Hitung mundur peluncuran roket
+#include <iostream>
+
+int main() {
+
+    //DEKLARASI
+    int i;
+
+    //ALGORITMA
+    for(i=10; i>=0; i--) {
+        std::cout<<i<<"\n";           
+        for(int p=1; p<=1000000000; p++) {
+            //kosong
+        }
+    }
+    std::cout<<"GO!\n";
+
+    return 0;
+
+}
