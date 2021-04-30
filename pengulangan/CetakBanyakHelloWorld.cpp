089e815f36b3bfa00ded802bdf307b850f5b0ab5
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-30 13:20:27 +0700

    code

---
+++ pengulangan/CetakBanyakHelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    //DEKLARASI
+    int i;  //pencacah pengulangan
+    int N;  //jumlah pengulangan
+
+    //ALGORITMA
+    std::cin>>N;
+    for(i=1; i<=N; i++) {
+        std::cout<<"Hello World!\n";
+    }
+
+    return 0;
+    
+}
