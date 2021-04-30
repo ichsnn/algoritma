3677ec61bc48d66a4ab6d8a90a684dd369370137
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-04-30 14:17:06 +0700

    code

---
+++ pengulangan/konstruksiWhile/CetakBarisan.cpp
@@ -0,0 +1,17 @@
+//Mencetak 1, 2, ..., N; nilai N ditentukan terlebih dahulu
+#include <iostream>
+
+int main() {
+
+    //DEKLARASI;
+    int i;
+    int N;
+
+    //ALGORITMA
+    std::cin>>N;
+    i=1;
+    while(i<=N) {
+        std::cout<<i<<"\n";
+        i++;
+    }
+}
