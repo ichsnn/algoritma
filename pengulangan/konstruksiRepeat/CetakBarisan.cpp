d84831134ba1ab895a40d7fcdf71ed771afcb583
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-05-01 16:38:55 +0700

    code

---
+++ pengulangan/konstruksiRepeat/CetakBarisan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    //DEKLARASI
+    int i;
+    int N;
+
+    //ALGORITMA
+    std::cin>>N;
+    i=1;
+    do {
+        std::cout<<i<<" ";
+        i++;
+    } while (i<=N);
+
+    return 0;
+    
+}
