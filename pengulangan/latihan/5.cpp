07d98d58735b6fdf84d24c93e40ebd2c69ad2cc4
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-05-05 20:01:34 +0700

    code

--- pengulangan/latihan/5.cpp
+++ pengulangan/latihan/5.cpp
@@ -5,11 +5,22 @@ using namespace std;
 int main() {
 
     //DEKLARASI
-    const int sum = 25;
-    int N;
+    const int sum = 25;    
     int x, y, z;    
     int counter;
 
     //ALGORITMA
-    
+    cout<<"Solusi Persamaan x + y + z = 25"<<endl;
+    cout<<"x y z"<<endl;
+    for(x=1;x<=sum;x++) {
+        for(y=1;y<=sum;y++) {
+            for(z=1;z<=sum;z++) {
+                if(x + y + z == sum) {
+                    cout<<x<<" "<<y<<" "<<z<<endl;
+                }
+            }            
+        }        
+    }
+
+    return 0;
 }
