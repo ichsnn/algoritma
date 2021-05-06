7bb649402e2253c818b7b8cddd875ef0fb8a415a
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-05-06 20:29:16 +0700

    i dont know how to convert number to words without array

--- prosedure/latihan/5.cpp
+++ prosedure/latihan/5.cpp
@@ -9,7 +9,7 @@ void kata(int bil);
 main() {
 
     //DEKLARASI
-    int x;
+    int x;    
 
     //PROGRAM UTAMA
     cout<<"Masukkan Bilangan Positif";cin>>x;
@@ -18,44 +18,43 @@ main() {
 
 void kata(int bil){
 
-    //DEKLARASI
-    string luaran;
+    //DEKLARASI    
+    struct satuan {
+        string satu = "satu";
+        string dua = "dua";
+        string tiga = "tiga";
+        string empat = "empat";
+        string lima = "lima";
+        string enam = "enam";
+        string tujuh = "tujuh";
+        string delapan = "delapan";
+        string sembilan = "sembilan";  
+        string sepuluh = "sepuluh";
+        string sebelas = "sebelas";
+        string belas = "belas";
+        string puluh = "puluh";
+        string seratus = "seratus";
+        string ratus = "ratus";
+        string ribu = "ribu";
+        string seribu = "seribu";
+    };   
+    satuan angka;
+    string luaran;    
 
-    //ALGORITMA
-    if(bil<0) {
-        cout<<"Masukkan salah";
-    } else {
-        if(bil == 0) {
-            string nol = "nol";
-        }
-        if(bil == 1) {
-            string satu = "satu";
-        }
-        if(bil == 2) {
-            string dua = "dua";
-        }
-        if(bil == 3) {
-            string tiga = "dua";
-        }
-        if(bil == 4) {
-            string empat = "dua";
-        }
-        if(bil == 5) {
-            string lima = "dua";
-        }
-        if(bil == 6) {
-            string enam = "dua";
-        }
-        if(bil == 7) {
-            string tujuh = "dua";
-        }
-        if(bil == 8) {
-            string delapan = "dua";
-        }
-        if(bil == 9) {
-            string dua = "dua";
-        }
-
-
-    }
+    //ALGORITMA        
+    if( bil == 1000) luaran += angka.seribu;    
+    if(bil == 100) luaran += angka.seratus;
+    if(bil == 11) luaran += angka.sebelas;
+    if(bil == 10) luaran += angka.sepuluh;
+    if(bil == 9) luaran += angka.sembilan;
+    if(bil == 8) luaran += angka.delapan;
+    if(bil == 7) luaran += angka.tujuh;
+    if(bil == 6) luaran += angka.enam;
+    if(bil == 5) luaran += angka.lima;
+    if(bil == 4) luaran += angka.empat;
+    if(bil == 3) luaran += angka.tiga;
+    if(bil == 2) luaran += angka.dua;
+    if(bil == 1) luaran += angka.satu;
+    
+    cout<<luaran;
 }
