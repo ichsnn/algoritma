746ed1367641dff6d0065b0112905ea856ea5a87
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-05-07 15:30:54 +0700

    code

---
+++ prosedure/latihan/12.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+//DEKLARASI PURWARUPA PROSEDUR YANG DIGUNAKAN
+void piramid(int N);
+
+main() {
+
+    //DEKLARASI
+    int n;
+
+    //PROGRAM UTAMA
+    cout<<"Masukkan tinggi : ";cin>>n;
+    piramid(n);
+    
+}
+
+void piramid(int N){
+
+    //DEKLARASI
+    int i, j;
+
+    //ALGORITMA
+    for(i=1;i<=N;i++) {
+        for(j=i;j<=N;j--) {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
