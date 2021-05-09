077892a6659dc8725cead993c9edb3057d72f48d
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-05-09 22:00:58 +0700

    code

--- fungsi/latihan/1.cpp
+++ fungsi/latihan/1.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 double mutlak(double x);
+double ganda(double x);
+bool genap(int x);
 
 main() {
 
@@ -11,10 +13,22 @@ main() {
 
     //PROGRAM UTAMA
     cout<<"Masukkan x : ";cin>>x;    
-    cout<<"Nilai |x| adalah "<<mutlak(x);
+    cout<<"Nilai |x| adalah "<<mutlak(x)<<endl;
+    cout<<"Nilai 2 kali x adalah "<<ganda(x)<<endl;
+    if(genap(x)) {
+        cout<<"Nilai x adalah genap"<<endl;
+    } else cout<<"Nilai x adalah ganjil"<<endl;
 
 }
 
 double mutlak(double x) {
     return x<0 ? -x : x;
+}
+
+double ganda(double x) {
+    return 2*x;
+}
+
+bool genap(int x) {
+    return x%2==0 ? true : false;
 }
