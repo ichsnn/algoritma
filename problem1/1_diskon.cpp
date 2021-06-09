e2162be6e5fd57b587dca864902b04725514b61b
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-06-09 12:15:07 +0700

    code

--- problem1/1_diskon.cpp
+++ problem1/1_diskon.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //DEKLARASI GLOBAL
-const Nmaks = 100;
+const int Nmaks = 100;
 struct barang {
     int kodeBrg;
     double hargaBrg;
@@ -11,7 +11,8 @@ struct barang {
 typedef barang LarikBarang[Nmaks+1];
 
 //DEKLARASI PURWARUPA PROCEDURE YANG AKAN DIGUNAKAN
-procedure turunkanHarga(LarikBarang &hargaJual, float p, int N);
+void listHarga(LarikBarang hargaJual, int N);
+void turunkanHarga(LarikBarang &hargaJual, float p, int N);
 
 //PROGRAM UTAMA
 int main(int argc, char const *argv[])
@@ -22,13 +23,44 @@ int main(int argc, char const *argv[])
     
     //ALGORITMA
     hargaJual_[1].hargaBrg = 50000;
+    hargaJual_[1].kodeBrg = 24312; 
     hargaJual_[2].hargaBrg = 40000;
-    hargaJual_[3].hargaBrg = 20000;            
+    hargaJual_[2].kodeBrg = 24313;
+    hargaJual_[3].hargaBrg = 20000; 
+    hargaJual_[3].kodeBrg = 24314;
+    hargaJual_[4].hargaBrg = 30000;
+    hargaJual_[4].kodeBrg = 24315;
+    hargaJual_[5].hargaBrg = 35000;
+    hargaJual_[5].kodeBrg = 24316;
+
+    banyakData = 5;
+
+    //Menmapilkan daftar harga
+    cout<<endl;
+    cout<<"Daftar Harga Barang Sebelum Diturunkan"<<endl;
+    cout<<"----------------------------------------------"<<endl;
+    listHarga(hargaJual_, banyakData);
+    cout<<endl;
+    cout<<"Daftar Harga Barang Setelah Diturunkan"<<endl;
+    cout<<"----------------------------------------------"<<endl;
+    //Menurunkan harga 5%;
+    turunkanHarga(hargaJual_, 5, banyakData);
+    listHarga(hargaJual_, banyakData);  //tampilkan setelah diturunkan
 
     return 0;
 }
 
-procedure turunkanHarga(LarikBarang &hargaJual, float p, int N) {
+void listHarga(LarikBarang hargaJual, int N) {
+    //DEKLARASI
+    int i;
+    //ALGORITMA           
+    cout<<"Kode Barang    Harga Barang"<<endl;
+    for(i=1;i<=N;i++) {
+        cout<<hargaJual[i].kodeBrg<<"          Rp."<<hargaJual[i].hargaBrg<<endl;
+    }
+}
+
+void turunkanHarga(LarikBarang &hargaJual, float p, int N) {
     //DEKLARASI
     int i;
     float diskonHarga;
