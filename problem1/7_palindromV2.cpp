bb26a0a5b4c921313ab31a56849c0ed665dace3f
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-07-03 22:02:07 +0700

    palindrom V2

---
+++ problem1/7_palindromV2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// FUNCTION
+bool palindrom(string huruf, int N)
+{
+    // Declaration
+    int i;
+    int j;
+    bool pal;
+    //Algorithm
+    i = 0;
+    j = N - 1;
+    pal = true;
+    while (i <= j && pal)
+    {
+        if (huruf[i] != huruf[j])
+            pal = false;
+        else
+        {
+            i++;
+            j--;
+        }
+    }
+    return pal;
+}
+
+// MAIN PROGRAM
+int main(int argc, char const *argv[])
+{
+    // Declaration 
+    string kata;
+
+    // Program
+    cout<<"Masukkan Kata : ";getline(cin, kata);
+    if(palindrom(kata, kata.length()))
+        cout<<"Palindrom";
+    else
+        cout<<"Bukan Palindrom";
+    return 0;
+}
